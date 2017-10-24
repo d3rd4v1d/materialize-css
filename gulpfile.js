54d01a1ef9408cd2ad57bbec6729e99f59dee61b
David Schneider <d4vid81@gmail.com>
2017-10-24 11:43:45 +0200

    Fixing JS compilation error, adding Roboto font to build, and steamlining build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,83 @@
 // grab our gulp packages
-var gulp  = require('gulp'),
-  cleanCSS = require('gulp-clean-css'),
-  cleanJS = require('gulp-minify'),
-  concat = require('gulp-concat'),
-  sass   = require('gulp-sass'),
-  util = require('gulp-util');
-
-// sources
-var jsSource = 'node_modules/materialize-css/js/*.js',
-  cssSource = 'node_modules/materialize-css/sass/materialize.scss';
+var gulp      = require('gulp'),
+  cleanCSS    = require('gulp-clean-css'),
+  cleanJS     = require('gulp-minify'),
+  concat      = require('gulp-concat'),
+  sass        = require('gulp-sass'),
+  sourcemaps  = require('gulp-sourcemaps'),
+  util        = require('gulp-util');
 
 // default task
 gulp.task('default', function() {
   util.log('Complete Build');
+  gulp.start('copy-font');
   gulp.start('build-css');
   gulp.start('build-js');
-});
-
-// default task
-gulp.task('production', function() {
-  util.log('Complete Production Build');
-  gulp.start('build-css').start('clean-css');
-  gulp.start('build-js').start('clean-js');
 });
 
 // build css
 gulp.task('build-css', function() {
   util.log('Building CSS files!');
-  return gulp.src([cssSource, './scss/settings.scss'], { base: '.' })
+  return gulp.src(['node_modules/materialize-css/sass/materialize.scss', './scss/settings.scss'], { base: '.' })
     .pipe(sass())
-    .pipe(concat('app.css'))
-    .pipe(gulp.dest('build/css'));
-});
-
-// compress css
-gulp.task('clean-css', function(){
-  util.log('Compressing CSS files!');
-  return gulp.src('build/css/app.css')
     .pipe(concat('app.min.css'))
+    .pipe(sourcemaps.init())
     .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('build/css'));
 });
 
 // build js
 gulp.task('build-js', function() {
   util.log('Building JS files!');
-  return gulp.src([jsSource, './js/app.js'], { base: '.' })
+  return gulp.src([
+      'node_modules/jquery/dist/jquery.js',
+      "node_modules/materialize-css/js/initial.js",
+      "node_modules/materialize-css/js/jquery.easing.1.4.js",
+      "node_modules/materialize-css/js/animation.js",
+      "node_modules/materialize-css/js/velocity.min.js",
+      "node_modules/materialize-css/js/hammer.min.js",
+      "node_modules/materialize-css/js/jquery.hammer.js",
+      "node_modules/materialize-css/js/global.js",
+      "node_modules/materialize-css/js/collapsible.js",
+      "node_modules/materialize-css/js/dropdown.js",
+      "node_modules/materialize-css/js/modal.js",
+      "node_modules/materialize-css/js/materialbox.js",
+      "node_modules/materialize-css/js/parallax.js",
+      "node_modules/materialize-css/js/tabs.js",
+      "node_modules/materialize-css/js/tooltip.js",
+      "node_modules/materialize-css/js/waves.js",
+      "node_modules/materialize-css/js/toasts.js",
+      "node_modules/materialize-css/js/sideNav.js",
+      "node_modules/materialize-css/js/scrollspy.js",
+      "node_modules/materialize-css/js/forms.js",
+      "node_modules/materialize-css/js/slider.js",
+      "node_modules/materialize-css/js/cards.js",
+      "node_modules/materialize-css/js/chips.js",
+      "node_modules/materialize-css/js/pushpin.js",
+      "node_modules/materialize-css/js/buttons.js",
+      "node_modules/materialize-css/js/transitions.js",
+      "node_modules/materialize-css/js/scrollFire.js",
+      "node_modules/materialize-css/js/date_picker/picker.js",
+      "node_modules/materialize-css/js/date_picker/picker.date.js",
+      "node_modules/materialize-css/js/date_picker/picker.time.js",
+      "node_modules/materialize-css/js/character_counter.js",
+      "node_modules/materialize-css/js/carousel.js",
+      "node_modules/materialize-css/js/tapTarget.js",
+      './js/app.js'
+    ], { base: '.' })
     .pipe(concat('app.js'))
+    .pipe(cleanJS({
+      ext:{
+        min:'.min.js'
+      }
+    }))
     .pipe(gulp.dest('build/js'));
 });
 
-// compress js
-gulp.task('clean-js', function() {
-  util.log('Compressing JS files!');
-  gulp.src('build/js/app.js')
-    .pipe(cleanJS({
-        ext:{
-            min:'.min.js'
-        }
-    }))
-    .pipe(gulp.dest('build/js'))
+// copy fonts
+gulp.task('copy-font', function() {
+  util.log('Copying Roboto font files!');
+  return gulp.src('node_modules/materialize-css/dist/fonts/roboto/*')
+      .pipe(gulp.dest('build/fonts/roboto'));
 });
